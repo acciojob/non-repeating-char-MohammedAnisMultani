c79e6ac55a589be89710d1dd24785505b02172f5
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-23 19:18:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+for(let i=0 ; i<str.length-1; i++){
+    let value = str.charAt(i);
+    let isfound = false;
+    for(let j=0; j<str.length-1; j++){
+        if(i!=j && value == str.charAt(j)){
+            isfound = true;
+        }
+       
+    }
+    if(isfound == false){
+        return value; 
+       
+    }
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
